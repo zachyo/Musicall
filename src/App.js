8b73b6308f955d77efb1d880b2863d888807d287
zacheusIsrael <zacheazy99@yahoo.com>
2022-10-19 13:13:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage';
 
+import {Route, Routes} from 'react-router-dom'
+import Overview from './pages/overview/overview';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Homepage />
-      </header>
+      <Routes>
+        <Route exact path='' element={<Overview/>} >
+          <Route path='/' element={<Homepage/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
