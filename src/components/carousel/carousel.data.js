3b4be44e4540715fa5e53a3b27de9ad67ab14b21
zacheusIsrael <zacheazy99@yahoo.com>
2022-10-22 00:31:53 +0100

    Create carousel.data.js

---
+++ src/components/carousel/carousel.data.js
@@ -0,0 +1,50 @@
+const CARDS_DATA = [
+  {
+    id: 1,
+    name: "Robbert Smith",
+    occupation: "CEO, Salesy",
+    testimony:
+      "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+  },
+  {
+    id: 2,
+    name: "Robbert Smith",
+    occupation: "CEO, Salesy",
+    testimony:
+      "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+  },
+  // {
+  //   id: 3,
+  //   name: "Robbert Smith",
+  //   occupation: "CEO, Salesy",
+  //   image: img2,
+  //   testimony:
+  //     "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Robbert Smith",
+  //   occupation: "CEO, Salesy",
+  //   image: img2,
+  //   testimony:
+  //     "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+  // },
+  // {
+  //   id: 5,
+  //   name: "Robbert Smith",
+  //   occupation: "CEO, Salesy",
+  //   image: img2,
+  //   testimony:
+  //     "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+  // },
+
+  {
+    id: 7,
+    name: "Robbert Smith",
+    occupation: "CEO, Salesy",
+    testimony:
+      "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+  },
+];
+
+export default CARDS_DATA;
