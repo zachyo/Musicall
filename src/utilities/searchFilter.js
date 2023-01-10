fd24ceebf06db37f5076631079cc0c461636f375
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-10 17:57:01 +0100

    Create searchFilter.js

---
+++ src/utilities/searchFilter.js
@@ -0,0 +1,7 @@
+export const searchFilter = (searchKey, data) => {
+  let newData = data.filter(
+    (each) =>
+      each?.name.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
+  );
+  return newData;
+};
