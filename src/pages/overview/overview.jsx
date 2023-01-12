f06134d3f3130fda6102354a904a8e03e170968a
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-12 23:05:47 +0100

    searchkey changes

--- src/pages/overview/overview.jsx
+++ src/pages/overview/overview.jsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import PlayerControl from "../../components/player-control/player-control";
 import SearchBar from "../../components/search-bar/search-bar";
 import SideNav from "../../components/side-nav/side-nav";
+import SearchContext from "../../contexts/searchContext";
+
 
 const Overview = () => {
+  const [searchKey, setSearchKey] = useState(undefined);
+  const setSearch = () => setSearchKey(searchKey)
+  console.log(searchKey);
+
   return (
     <div className="overview">
       <div className="flex">
-        <SideNav />
+        <SearchContext.Provider value={{ searchKey, setSearch }}>
+          <SideNav />
+        </SearchContext.Provider>
 
         <div className="overview-container">
           <SearchBar />
