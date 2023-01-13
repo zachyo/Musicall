dc8f4abca20b2c5f4072f606bf26cbd2140750f8
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-13 10:18:32 +0100

    carousel update

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,64 +1,32 @@
-// import CARDS_DATA from "./carousel.data";
-import R14 from '../../assets/images/Rectangle 14.png'
-import R15 from '../../assets/images/Rectangle 15.png'
-import R16 from '../../assets/images/Rectangle 16.png'
-import R17 from '../../assets/images/Rectangle 17.png'
-import R18 from '../../assets/images/Rectangle 18.png'
-import R19 from '../../assets/images/Rectangle 19.png'
-import R20 from '../../assets/images/Rectangle 20.png'
-import R21 from "../../assets/images/Rectangle 21.png";
-import './carousel.scss'
+import CARDS_DATA from "./carousel.data";
+
+import "./carousel.scss";
+import { useContext } from "react";
+import SearchContext from "../../contexts/searchContext";
+import { searchFilter } from "../../utilities/searchFilter";
 
 const Carousel = () => {
-
+  const {searchKey} = useContext(SearchContext)
+  let newData = CARDS_DATA
+  if (searchKey) {
+    newData = searchFilter(searchKey, CARDS_DATA);
+    console.log(searchKey, newData);
+  }
+  const songs = newData.map((each) => {
     return (
-      <div className="carousel flex text-light text-left">
-        <div className="releaseCard">
-          <img src={R14} alt="" />
-          <h2>Life in a circle</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R15} alt="" />
-          <h2>Mountain</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R16} alt="" />
-          <h2>Limits</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R17} alt="" />
-          <h2>Everything's Black</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R19} alt="" />
-          <h2>Cancelled</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R20} alt="" />
-          <h2>Named</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R18} alt="" />
-          <h2>Blind Eyes</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R21} alt="" />
-          <h2>Seasons In</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R14} alt="" />
-          <h2>Life in a circle</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R15} alt="" />
-          <h2>Mountain</h2>
-        </div>
-        <div className="releaseCard">
-          <img src={R16} alt="" />
-          <h2>Limits</h2>
-        </div>
+      <div className="releaseCard" key={each.id}>
+        <img src={each.img} alt="" />
+        <h2>{each.title}</h2>
       </div>
     );
-}
- 
+  });
+  
+
+  return (
+    <div className="carousel flex text-light text-left">
+      {songs}
+    </div>
+  );
+};
+
 export default Carousel;
