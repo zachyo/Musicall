1f617aab34097583a5e515aabd4a6cba7000b23b
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-13 10:17:53 +0100

    carousel data updated

--- src/components/carousel/carousel.data.js
+++ src/components/carousel/carousel.data.js
@@ -1,49 +1,67 @@
+import R14 from "../../assets/images/Rectangle 14.png";
+import R15 from "../../assets/images/Rectangle 15.png";
+import R16 from "../../assets/images/Rectangle 16.png";
+import R17 from "../../assets/images/Rectangle 17.png";
+import R18 from "../../assets/images/Rectangle 18.png";
+import R19 from "../../assets/images/Rectangle 19.png";
+import R20 from "../../assets/images/Rectangle 20.png";
+import R21 from "../../assets/images/Rectangle 21.png";
+
 const CARDS_DATA = [
   {
     id: 1,
-    name: "Robbert Smith",
-    occupation: "CEO, Salesy",
-    testimony:
-      "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+    title: "Life in a circle",
+    img: R14,
   },
   {
     id: 2,
-    name: "Robbert Smith",
-    occupation: "CEO, Salesy",
-    testimony:
-      "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+    title: "Mountain",
+    img: R15,
   },
-  // {
-  //   id: 3,
-  //   name: "Robbert Smith",
-  //   occupation: "CEO, Salesy",
-  //   image: img2,
-  //   testimony:
-  //     "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
-  // },
-  // {
-  //   id: 4,
-  //   name: "Robbert Smith",
-  //   occupation: "CEO, Salesy",
-  //   image: img2,
-  //   testimony:
-  //     "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
-  // },
-  // {
-  //   id: 5,
-  //   name: "Robbert Smith",
-  //   occupation: "CEO, Salesy",
-  //   image: img2,
-  //   testimony:
-  //     "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
-  // },
-
+  {
+    id: 3,
+    title: "Limits",
+    img: R16,
+  },
+  {
+    id: 4,
+    title: "Everything's Black",
+    img: R17,
+  },
+  {
+    id: 5,
+    title: "Blind Eyes",
+    img: R18,
+  },
+  {
+    id: 6,
+    title: "Cancelled",
+    img: R19,
+  },
   {
     id: 7,
-    name: "Robbert Smith",
-    occupation: "CEO, Salesy",
-    testimony:
-      "Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up. Generate a unique link that directs your audience to your products or store. This helps build more confidence and drives your sales up.",
+    title: "Named",
+    img: R20,
+  },
+  {
+    id: 8,
+    title: "Seasons In",
+    img: R21,
+  },
+  {
+    id: 9,
+    title: "Limits",
+    img: R16,
+  },
+  {
+    id: 10,
+    title: "Life in a circle",
+    img: R14,
+  },
+  {
+    id: 11,
+    title: "Mountain",
+    img: R15,
   },
 ];
 
