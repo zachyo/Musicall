8ec29fb79359bde52315e2035a7a6971e2165ba4
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-13 10:22:11 +0100

    overview.jsx update

--- src/pages/overview/overview.jsx
+++ src/pages/overview/overview.jsx
@@ -1,32 +1,32 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 import PlayerControl from "../../components/player-control/player-control";
 import SearchBar from "../../components/search-bar/search-bar";
 import SideNav from "../../components/side-nav/side-nav";
-import SearchContext from "../../contexts/searchContext";
-
+import { SearchProvider } from "../../contexts/searchContext";
 
 const Overview = () => {
-  const [searchKey, setSearchKey] = useState(undefined);
-  const setSearch = () => setSearchKey(searchKey)
-  console.log(searchKey);
+  // const [searchKey, setSearchKey] = useState(undefined);
+  // const setSearch = () => setSearchKey(searchKey);
+  // console.log(searchKey);
 
   return (
-    <div className="overview">
-      <div className="flex">
-        <SearchContext.Provider value={{ searchKey, setSearch }}>
+    <SearchProvider>
+      <div className="overview">
+        <div className="flex">
           <SideNav />
-        </SearchContext.Provider>
 
-        <div className="overview-container">
-          <SearchBar />
-          <div className="overview-replaceable">
-            <Outlet />
+          <div className="overview-container">
+            <SearchBar />
+
+            <div className="overview-replaceable">
+              <Outlet />
+            </div>
           </div>
         </div>
+        <PlayerControl />
       </div>
-      <PlayerControl />
-    </div>
+    </SearchProvider>
   );
 };
 
