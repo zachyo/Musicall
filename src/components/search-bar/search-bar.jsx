394f52395c43a640e0e08e799f1419f47cbfa9b6
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-13 10:19:25 +0100

    search-bar updated

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,26 +1,16 @@
 import { useContext } from "react";
-import { useState } from "react";
 import search from "../../assets/icons/search.svg";
-import SearchContext  from "../../contexts/searchContext";
+import SearchContext from "../../contexts/searchContext";
 
 const SearchBar = () => {
-  const { setSearch } = useContext(SearchContext);
-  // const [searchKey, setSearchKey] = useState(undefined);
+  const { handleChange } = useContext(SearchContext);
 
-  //searching system
-  const handleChange = (event) => {
-    console.log(setSearch)
-    // setSearch(event.target.value);
-  };
-
-  // console.log(searchKey, searchVal);
   return (
     <div className="search-bar flex px-6 my-4 space-x-5">
       <img src={search} alt="" />
       <input
         type="text"
         placeholder="Search artists..."
-        // value={searchKey}
         onChange={handleChange}
         className="bg-dark2 w-full text-light p-2"
       />
