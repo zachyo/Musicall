d4db0fb7c021178c3a99e92d6b1bcb03cc99b4a4
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-13 10:22:38 +0100

    search utility

--- src/utilities/searchFilter.js
+++ src/utilities/searchFilter.js
@@ -1,7 +1,8 @@
 export const searchFilter = (searchKey, data) => {
+  
   let newData = data.filter(
     (each) =>
-      each?.name.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
+      each?.title.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
   );
   return newData;
 };
