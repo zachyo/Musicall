45755c8040351d44bf9055b0e58b269ce96da80a
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-16 01:16:11 +0100

    player-control update

--- src/components/player-control/player-control.jsx
+++ src/components/player-control/player-control.jsx
@@ -4,19 +4,23 @@ import play from "../../assets/icons/Vector (3).svg";
 import next from "../../assets/icons/next.svg";
 import repeat from "../../assets/icons/repeate-one.svg";
 import volume from '../../assets/icons/volume-high.svg'
-import R21 from "../../assets/images/Rectangle 21.png";
+
+import './player-control.scss'
+import { useContext } from "react";
+import NowPlayingContext from "../../contexts/nowPlayingContext";
 
 const PlayerControl = () => {
+  const {nowPlaying} = useContext(NowPlayingContext)
   return (
     <div className="player-control flex h-24 bg-black fixed bottom-0 z-10 w-full text-white p-10 items-center justify-around">
       <div className="song-details flex items-center">
-        <img src={R21} alt="" className="h-12 mr-4 rounded-xl" />
+        <img src={nowPlaying.img} alt="" className="h-12 mr-4 rounded-xl" />
         <div className="text-left">
-          <h1>Seasons In</h1>
+          <h1>{nowPlaying.title}</h1>
           <small className="text-lightWhite">James</small>
         </div>
       </div>
-      <div className="w-2/5">
+      <div className="buttons flex flex-col items-center justify-center space-y-5">
         <div className="flex space-x-14 items-center justify-center">
           <img src={shuffle} alt="shuffle" className="h-6" />
           <img src={previous} alt="previous" className="h-8" />
@@ -24,8 +28,14 @@ const PlayerControl = () => {
           <img src={next} alt="next" className="h-8" />
           <img src={repeat} alt="repeat" className="h-8" />
         </div>
+        <span className="line">u</span>
+        {/* <div className="track-length">lj</div> */}
       </div>
-      <img src={volume} alt="volume" />
+      <div className="volume flex items-center justify-center">
+        <img src={volume} alt="volume" className="mr-3"/>
+        <span className="circle"></span>
+        <span className="line">u</span>
+      </div>
     </div>
   );
 };
