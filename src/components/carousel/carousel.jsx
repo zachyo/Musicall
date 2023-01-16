2594287f481dbeb42c4c33c6542b2251308cd933
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-16 01:15:42 +0100

    carousel

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -2,11 +2,14 @@ import CARDS_DATA from "./carousel.data";
 
 import "./carousel.scss";
 import { useContext } from "react";
+import {Link} from 'react-router-dom'
 import SearchContext from "../../contexts/searchContext";
 import { searchFilter } from "../../utilities/searchFilter";
+import NowPlayingContext from "../../contexts/nowPlayingContext";
 
 const Carousel = () => {
   const {searchKey} = useContext(SearchContext)
+  const {handleClick} = useContext(NowPlayingContext)
   let newData = CARDS_DATA
   if (searchKey) {
     newData = searchFilter(searchKey, CARDS_DATA);
@@ -14,10 +17,10 @@ const Carousel = () => {
   }
   const songs = newData.map((each) => {
     return (
-      <div className="releaseCard" key={each.id}>
+      <Link to={each.type === 'album' ? `album/${each.title}` : ''} className="releaseCard" key={each.id} onClick={each.type === 'album' ? {} : ()=>handleClick(each)}>
         <img src={each.img} alt="" />
         <h2>{each.title}</h2>
-      </div>
+      </Link>
     );
   });
   
