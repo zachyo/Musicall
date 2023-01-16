e02bad46d81fd00a875c97e29d18fb43746ffc7a
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-16 01:14:34 +0100

    app update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Homepage from './pages/homepage/homepage';
 
 import {Route, Routes} from 'react-router-dom'
 import Overview from './pages/overview/overview';
+import ChartAlbum from './pages/chart-album/chart-album';
+
+import leadImage from './assets/images/Lead-image.png'
+import Collections from './pages/collections/collections';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='' element={<Overview/>} >
-          <Route path='/' element={<Homepage/>} />
+        <Route exact path="" element={<Overview chartB={leadImage} />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="album" element={<ChartAlbum />}>
+            <Route path=":id" element={<Collections />} />
+          </Route>
+          <Route path="collections" element={<Collections />}/>
         </Route>
       </Routes>
     </div>
