2a7641c41c307b20fae30e94e083fa0d5df4c78f
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-17 01:24:12 +0100

    side-nav cleanup

--- src/components/side-nav/side-nav.jsx
+++ src/components/side-nav/side-nav.jsx
@@ -10,24 +10,24 @@ import {Link} from 'react-router-dom'
 
 const SideNav = () => {
 
-    const CustomIcon = ({iconn}) => {
+    const CustomIcon = ({iconn,to}) => {
         return (
-          <Link to="#" className='m-4'>
+          <Link to={to ? to : '#'} className='m-4'>
             <img src={iconn} alt="" className="" />
           </Link>
         );
     }
   return (
     <div className="side-nav w-1/12  flex flex-col items-center mr-2 mt-5">
-      <div className="side-nav_logo  mb-12">
+      <Link to="/" className="side-nav_logo  mb-10">
         <img src={logo} alt="" />
-      </div>
+      </Link>
       <div className="side-nav_links">
         <div className="side-nav_top mb-6 flex flex-col items-center justify-center bg-black rounded-full py-5 px-1">
-          <CustomIcon iconn={home} />
-          <CustomIcon iconn={musicLibrary} />
+          <CustomIcon iconn={home} to={"/"} />
+          <CustomIcon iconn={musicLibrary} to={"album"} />
           <CustomIcon iconn={radio} />
-          <CustomIcon iconn={video} />
+          <CustomIcon iconn={video} to={"collections"} />
         </div>
         <div className="side-nav_down flex flex-col items-center justify-center bg-black rounded-full py-5 px-1">
           <CustomIcon iconn={profile} />
