5fb47b84ee4fc543f4a0ed091f11455f7cc5b067
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-17 01:27:58 +0100

    nowPlaying context

---
+++ src/contexts/nowPlayingContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import R14 from "../assets/images/Rectangle 14.png";
+
+
+const NowPlayingContext = createContext();
+
+export const NowPlayingContextProvider = ({ children }) => {
+  const [nowPlaying, setNowPlaying] = useState({
+    id: 1,
+    title: "Life in a circle",
+    img: R14,
+  });
+  //   console.log(nowPlaying)
+  //searching system
+  const handleClick = (song) => {
+    setNowPlaying(song);
+  };
+  return (
+    <NowPlayingContext.Provider value={{ nowPlaying, handleClick }}>
+      {children}
+    </NowPlayingContext.Provider>
+  );
+};
+
+export default NowPlayingContext;
