feace996e464c22a2d1375be029069bc9887a00d
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-18 23:49:45 +0100

    collections update

--- src/pages/collections/collections.jsx
+++ src/pages/collections/collections.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import CARDS_DATA from "../../components/carousel/carousel.data";
+import "./collections.scss";
+
 const Collections = () => {
-    return ( <div className="collections-page text-light">This is the colllections page</div> );
-}
- 
+  const [option, setOption] = useState("mine");
+
+  const myCollection = CARDS_DATA.filter(
+    (song) => song.collection === option
+  ).map((song) => {
+    return (
+      <div className="badge" style={{ backgroundImage: `url(${song.img})` }}>
+        <h1>{song.title}</h1>
+        <p>John Watts</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className="collections-page text-light">
+      <div className="collections-nav text-left">
+        <button
+          className={option === "mine" ? "active" : ""}
+          onClick={() => {
+            setOption("mine");
+          }}
+        >
+          My Collection
+        </button>
+        <button
+          className={option === "liked" ? "active" : ""}
+          onClick={() => {
+            setOption("liked");
+          }}
+        >
+          Liked
+        </button>{" "}
+      </div>
+
+      <div className="flex mt-7">{myCollection}</div>
+    </div>
+  );
+};
+
 export default Collections;
