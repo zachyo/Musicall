252850b98d0d6ec25fecb4cfc15e77cef9274ef8
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-20 00:27:23 +0100

    Update carousel.data.js

--- src/components/carousel/carousel.data.js
+++ src/components/carousel/carousel.data.js
@@ -1,78 +1,94 @@
-import R14 from "../../assets/images/Rectangle 14.png";
-import R15 from "../../assets/images/Rectangle 15.png";
-import R16 from "../../assets/images/Rectangle 16.png";
-import R17 from "../../assets/images/Rectangle 17.png";
-import R18 from "../../assets/images/Rectangle 18.png";
-import R19 from "../../assets/images/Rectangle 19.png";
-import R20 from "../../assets/images/Rectangle 20.png";
-import R21 from "../../assets/images/Rectangle 21.png";
+import R14 from "../../assets/images/Rectangle14.png";
+import R15 from "../../assets/images/Rectangle15.png";
+import R16 from "../../assets/images/Rectangle16.png";
+import R17 from "../../assets/images/Rectangle17.png";
+import R18 from "../../assets/images/Rectangle18.png";
+import R19 from "../../assets/images/Rectangle19.png";
+import R20 from "../../assets/images/Rectangle20.png";
+import R21 from "../../assets/images/Rectangle21.png";
 
 const CARDS_DATA = [
   {
     id: 1,
     title: "Life in a circle",
     img: R14,
+    imgLink: "../../assets/images/Rectangle14.png",
     type: "single",
+    collection: "mine",
   },
   {
     id: 2,
     title: "Mountain",
     img: R15,
     type: "single",
+    collection: "liked",
   },
   {
     id: 3,
     title: "Limits",
     img: R16,
     type: "album",
+    collection: "liked",
   },
   {
     id: 4,
     title: "Everything's Black",
     img: R17,
     type: "single",
+    collection: "mine",
   },
   {
     id: 5,
     title: "Blind Eyes",
     img: R18,
     type: "album",
+    collection: "liked",
   },
   {
     id: 6,
     title: "Cancelled",
     img: R19,
     type: "single",
+    collection: "mine",
   },
   {
     id: 7,
     title: "Named",
     img: R20,
     type: "album",
+    option: "my collection",
+    collection: "liked",
   },
   {
     id: 8,
     title: "Seasons In",
     img: R21,
     type: "single",
+    option: "my collection",
+    collection: "mine",
   },
   {
     id: 9,
     title: "Limits",
     img: R16,
     type: "album",
+    option: "my collection",
+    collection: "mine",
   },
   {
     id: 10,
     title: "Life in a circle",
     img: R14,
     type: "single",
+    option: "my collection",
+    collection: "liked",
   },
   {
     id: 11,
     title: "Mountain",
     img: R15,
     type: "album",
+    collection: "mine",
   },
 ];
 
