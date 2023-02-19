906f6e9710db5670c112053c61219fec09f3b920
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-19 23:47:02 +0100

    Update carousel.jsx

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -10,15 +10,15 @@ import NowPlayingContext from "../../contexts/nowPlayingContext";
 const Carousel = ({songData}) => {
   const {searchKey} = useContext(SearchContext)
   const {handleClick} = useContext(NowPlayingContext)
-  let newData = CARDS_DATA;
+  let newData = songData;
   if (searchKey) {
     newData = searchFilter(searchKey, CARDS_DATA);
     console.log(searchKey, newData);
   }
-  const songs = newData.map((each) => {
+  const songs = newData?.map((each) => {
     return (
       <Link to={each.type === 'album' ? `album/${each.title}` : ''} className="releaseCard" key={each.id} onClick={each.type === 'album' ? {} : ()=>handleClick(each)}>
-        <img src={each.img} alt="" />
+        <img src={each.cover} alt="songCover" />
         <h2>{each.title}</h2>
       </Link>
     );
