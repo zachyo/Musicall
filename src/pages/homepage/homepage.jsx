b37c2c8b723b7f8d6d3a8f7b2ae85b034bc9da6a
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-20 23:39:21 +0100

    Update homepage.jsx

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,41 +2,20 @@ import ChartCard from "../../components/chart-card/chart-card";
 import "./homepage.scss";
 
 import heroImg from "../../assets/images/heroImg.png";
-import topC3 from "../../assets/images/Lead-image.png";
-import topC1 from '../../assets/images/topC2.png'
-import topC2 from "../../assets/images/topC3.png";
+
 import Carousel from "../../components/carousel/carousel";
 import useFetch from "../../utilities/useFetch";
+import cardsListData from "../../utilities/cardsData";
 
 const Homepage = () => {
-  const cardsList = [
-    {
-      key: 1,
-      img : topC1,
-      title: "Golden age of the 80s",
-      artiste: "Sean Swader",
-      time: "2:34:45",
-    },
-    {
-      key: 2,
-      img : topC2,
-      title: `Reggae "n" Blues`,
-      artiste: "DJ YK mule",
-      time: "1:02:42",
-    },
-    {
-      key: 3,
-      img : topC3,
-      title: "Tomorrow's tunes",
-      artiste: "Obi Datti",
-      time: "2:01:25",
-    },
-  ].map((card) => {
+  const cardsList = cardsListData.map((card) => {
     return <ChartCard key={card.key} card={card} />;
   });
 
-  const { loading, error, data } = useFetch("https://musica-api.up.railway.app/new");
-console.log(error)
+  const { loading, error, data } = useFetch(
+    "https://musica-api.up.railway.app/new"
+  );
+  console.log(data, error);
   return (
     <div className="outlet flex flex-col justify-center">
       <div className="text-left flex justify-center">
