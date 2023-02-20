dfcd0a3fc9024f5dad005a34302d4d964959fe9c
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-20 23:39:27 +0100

    Create cardsData.js

---
+++ src/utilities/cardsData.js
@@ -0,0 +1,29 @@
+import topC3 from "../assets/images/Lead-image.png";
+import topC1 from "../assets/images/topC2.png";
+import topC2 from "../assets/images/topC3.png";
+
+const cardsListData = [
+  {
+    key: 1,
+    img: topC1,
+    title: "Golden age of the 80s",
+    artiste: "Sean Swader",
+    time: "2:34:45",
+  },
+  {
+    key: 2,
+    img: topC2,
+    title: `Reggae "n" Blues`,
+    artiste: "DJ YK mule",
+    time: "1:02:42",
+  },
+  {
+    key: 3,
+    img: topC3,
+    title: "Tomorrow's tunes",
+    artiste: "Obi Datti",
+    time: "2:01:25",
+  },
+];
+
+export default cardsListData;
