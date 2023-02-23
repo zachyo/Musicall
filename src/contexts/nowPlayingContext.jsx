44a5d2920ebd45600375fea559a8fbc75fdfdb80
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-23 17:16:38 +0100

    Update nowPlayingContext.jsx

--- src/contexts/nowPlayingContext.jsx
+++ src/contexts/nowPlayingContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import R14 from "../assets/images/Rectangle14.png";
+import ControlsContext from "./controlsContext";
 
 const NowPlayingContext = createContext();
 
@@ -8,14 +9,55 @@ export const NowPlayingContextProvider = ({ children }) => {
     id: 1,
     title: "Life in a circle",
     img: R14,
+    artist: { name: "James" },
   });
-  //   console.log(nowPlaying)
-  //searching system
-  const handleClick = (song) => {
-    setNowPlaying(song);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songIndex, setSongIndex] = useState(0);
+
+  const handleNowPlaying = (song, i) => {
+    if (song.title === nowPlaying.title) {
+      setIsPlaying((prev) => !prev);
+    } else {
+      setNowPlaying(song);
+      setIsPlaying(true);
+      setSongIndex(i);
+    }
+  };
+  const handlePlayPause = () => {
+    setIsPlaying((prev) => !prev);
+  };
+  const handlePrevSong = (tracks, shuffle) => {
+    if (songIndex === 0) {
+      handleNowPlaying(tracks[tracks.length - 1], tracks.length - 1);
+    } else if (shuffle) {
+      const num = Math.floor(Math.random() * tracks.length);
+      handleNowPlaying(tracks[num], num);
+    } else {
+      handleNowPlaying(tracks[songIndex - 1], songIndex - 1);
+    }
+  };
+
+  const handleNextSong = (tracks, shuffle) => {
+    if (songIndex === tracks.length - 1) {
+      handleNowPlaying(tracks[0], 0);
+    } else if (shuffle) {
+      const num = Math.floor(Math.random() * tracks.length);
+      handleNowPlaying(tracks[num], num);
+    } else {
+      handleNowPlaying(tracks[songIndex + 1], songIndex + 1);
+    }
   };
   return (
-    <NowPlayingContext.Provider value={{ nowPlaying, handleClick }}>
+    <NowPlayingContext.Provider
+      value={{
+        nowPlaying,
+        isPlaying,
+        handleNowPlaying,
+        handlePlayPause,
+        handlePrevSong,
+        handleNextSong,
+      }}
+    >
       {children}
     </NowPlayingContext.Provider>
   );
