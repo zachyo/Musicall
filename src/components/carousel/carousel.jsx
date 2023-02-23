d9cbfbe95b6b9b3b99506bfc272e58acf83bc5dc
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-23 17:17:50 +0100

    Update carousel.jsx

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -4,26 +4,31 @@ import "./carousel.scss";
 import { useContext } from "react";
 import {Link} from 'react-router-dom'
 import SearchContext from "../../contexts/searchContext";
-import { searchFilter } from "../../utilities/searchFilter";
 import NowPlayingContext from "../../contexts/nowPlayingContext";
+import { searchFilter } from "../../utilities/searchFilter";
 
 const Carousel = ({songData}) => {
   const {searchKey} = useContext(SearchContext)
-  const {handleClick} = useContext(NowPlayingContext)
+  const { handleNowPlaying } = useContext(NowPlayingContext);
   let newData = songData;
   if (searchKey) {
     newData = searchFilter(searchKey, CARDS_DATA);
     console.log(searchKey, newData);
   }
-  const songs = newData?.map((each) => {
+  const songs = newData?.map((each,i) => {
     return (
-      <Link to={each.type === 'album' ? `album/${each.title}` : ''} className="releaseCard" key={each.id} onClick={each.type === 'album' ? {} : ()=>handleClick(each)}>
-        <img src={each.cover} alt="songCover" />
-        <h2>{each.title}</h2>
-      </Link>
+      <div
+        className="releaseCard"
+        key={each.id}
+        onClick={()=>handleNowPlaying(each,i)}
+      >
+        <img src={each.album.cover} alt="songCover" />
+        <h2 className="truncate">{each.title}</h2>
+        <p className="">{each?.artist?.name}</p>
+      </div>
     );
   });
-  
+  // to={each.type === 'album' ? `album/${each.title}` : null}
 
   return (
     <div className="carousel flex text-light text-left">
