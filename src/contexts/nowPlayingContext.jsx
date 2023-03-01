cdfd927397109da841866dbbf89f243da2f70efb
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-01 21:03:50 +0100

    Update nowPlayingContext.jsx

--- src/contexts/nowPlayingContext.jsx
+++ src/contexts/nowPlayingContext.jsx
@@ -8,8 +8,8 @@ export const NowPlayingContextProvider = ({ children }) => {
   const [nowPlaying, setNowPlaying] = useState({
     id: 1,
     title: "Life in a circle",
-    img: R14,
     artist: { name: "James" },
+    album : {cover : R14}
   });
   const [isPlaying, setIsPlaying] = useState(false);
   const [songIndex, setSongIndex] = useState(0);
