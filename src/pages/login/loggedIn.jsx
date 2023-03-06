98fb8b38564f5e27d67b2e3a9aebec2432b7c909
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-06 16:01:30 +0100

    Create loggedIn.jsx

---
+++ src/pages/login/loggedIn.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const LoggedIn = () => {
+  const user = localStorage.getItem("user");
+  return (
+    <div className="signin ">
+      <h1 className="mt-32 mb-12 text-4xl font-bold text-lightSteel">
+        Welcome to Musicall
+      </h1>
+      <p className="text-sandy text-xl">Hi {user?.name}, You're logged in.</p>
+      <p className="text-sandy text-xl">
+        Click{" "}
+        <Link to={"/"} className="underline italic">
+          here
+        </Link>{" "}
+        to get started.
+      </p>
+    </div>
+  );
+};
+
+export default LoggedIn;
