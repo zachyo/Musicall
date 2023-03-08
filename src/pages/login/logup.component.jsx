db2dd49ade8d1d0f9ae3f3e05eab7572495d445a
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-08 19:34:51 +0100

    Update logup.component.jsx

--- src/pages/login/logup.component.jsx
+++ src/pages/login/logup.component.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import FormInput from "../../components/form-input/form-input.component";
+import Spinner from "../../components/spinner/spinner.component";
 // import google from "../assets/icons/Google.png";
 // import facebook from "../assets/icons/Facebook.png";
 import "./signin.styles.scss";
@@ -18,6 +21,28 @@ const SignUp = () => {
   const [isSending, setIsSending] = useState(false);
   const { first_name, last_name, email, password, gender, username } = user;
   const navigate = useNavigate();
+  const notifySuccess = () =>
+    toast.info("Account Created", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  const notifyError = () =>
+    toast.error("Error Creating Account", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
 
   const clearUser = () => {
     Array.from(document.querySelectorAll("input")).forEach((input) => {
@@ -44,15 +69,23 @@ const SignUp = () => {
       .then((res) => {
         console.log("here");
         console.log(res);
+        if (!res.ok) {
+          notifyError();
+        }
         return res.json();
       })
       .then((data) => {
-        console.log("here2");
-        localStorage.setItem("user", data);
-        data.message === "User Created"
-          ? navigate("/")
-          : console.log(data.message);
-
+        if (data.message === "User Created") {
+          notifySuccess();
+        }
+        console.log(data);
+        localStorage.setItem("user", data.user);
+        setTimeout(function () {
+          setIsSending(false);
+          data.message === "User Created"
+            ? window.location.reload()
+            : console.log(data.message);
+        }, 2000);
         clearUser();
       });
   };
@@ -63,93 +96,96 @@ const SignUp = () => {
   };
 
   return (
-      <div className="signin">
-        <div className="signup">
-          <h1 className="mb-12 text-4xl font-bold text-lightSteel">
-            Create Account
-          </h1>
-          <form className="form" onSubmit={handleSubmit}>
-            <div className="inputs flex justify-center flex-wrap w-[51rem]">
-              <FormInput
-                type="text"
-                name="first_name"
-                label="First Name"
-                placeholder="First Name"
-                value={first_name}
+    <div className="signin">
+      <div className="signup">
+        <h1 className="mb-12 text-4xl font-bold text-lightSteel">
+          Create Account
+        </h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="inputs flex justify-center flex-wrap w-[51rem]">
+            <FormInput
+              type="text"
+              name="first_name"
+              label="First Name"
+              placeholder="First Name"
+              value={first_name}
+              onChange={handleChange}
+              required
+            />
+            <FormInput
+              type="text"
+              name="last_name"
+              label="Last Name"
+              placeholder="Last Name"
+              value={last_name}
+              onChange={handleChange}
+              required
+            />
+            <FormInput
+              type="email"
+              name="email"
+              label="Email Address"
+              placeholder="Email Address"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+            <FormInput
+              type="email"
+              name="username"
+              label="Username"
+              placeholder="Username"
+              value={username}
+              onChange={handleChange}
+              required
+            />
+            {/* //gender */}
+            <div className="group">
+              <select
+                name="gender"
+                className="form-input dropdown"
                 onChange={handleChange}
-                required
-              />
-              <FormInput
-                type="text"
-                name="last_name"
-                label="Last Name"
-                placeholder="Last Name"
-                value={last_name}
-                onChange={handleChange}
-                required
-              />
-              <FormInput
-                type="email"
-                name="email"
-                label="Email Address"
-                placeholder="Email Address"
-                value={email}
-                onChange={handleChange}
-                required
-              />
-              <FormInput
-                type="email"
-                name="username"
-                label="Username"
-                placeholder="Username"
-                value={username}
-                onChange={handleChange}
-                required
-              />
-              {/* //gender */}
-              <div className="group">
-                <select
-                  name="gender"
-                  className="form-input dropdown"
-                  onChange={handleChange}
-                  value={gender}
-                >
-                  <option>Gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="noAns">Prefer not to say</option>
-                </select>
-              </div>
-              <FormInput
-                type="password"
-                name="password"
-                label="Password"
-                placeholder="Password"
-                value={password}
-                onChange={handleChange}
-                required
-                pattern="[A-Za-z0-7]{8,}"
-                title="Password must include number and alphabet, no symbols"
-              />
+                value={gender}
+              >
+                <option>Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="noAns">Prefer not to say</option>
+              </select>
             </div>
-            {/* <p className="valid">Password must be 8 characters long</p> */}
-            <div className="TandT flex items-center mb-1">
-              <input type="checkbox" name="acceptance" required />
-              <p className="text-lightSteel">
-                {" "}
-                I Accept The <Link to="/" className="underline">Terms and Conditions</Link>
-              </p>
-            </div>
+            <FormInput
+              type="password"
+              name="password"
+              label="Password"
+              placeholder="Password"
+              value={password}
+              onChange={handleChange}
+              required
+              pattern="[A-Za-z0-7]{8,}"
+              title="Password must include number and alphabet, no symbols"
+            />
+          </div>
+          {/* <p className="valid">Password must be 8 characters long</p> */}
+          <div className="TandT flex items-center mb-1">
+            <input type="checkbox" name="acceptance" required />
+            <p className="text-lightSteel">
+              {" "}
+              I Accept The{" "}
+              <Link to="/" className="underline">
+                Terms and Conditions
+              </Link>
+            </p>
+          </div>
 
-            <button type="submit" disabled={isSending} onClick={handleSubmit}>
-              Create Account
-            </button>
-          </form>
-          {/* <p>
+          <button type="submit" disabled={isSending} onClick={handleSubmit}>
+            {isSending === true ? <Spinner /> : "Create Account"}
+          </button>
+        </form>
+        {/* <p>
             Already have an account?{"  "}
             <Link to="/signin">Sign In</Link>
           </p> */}
-          {/* <div className="otherSignIn">
+        {/* <div className="otherSignIn">
             <div className="line">
               <hr />
               <p>Or sign in with</p>
@@ -171,9 +207,9 @@ const SignUp = () => {
               </div>
             </div>
           </div> */}
-        </div>
       </div>
-    
+      <ToastContainer />
+    </div>
   );
 };
 
