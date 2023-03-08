b73ac3a089e129a9b66e7fddc7b0b0d835f50fab
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-08 19:35:16 +0100

    Create spinner.component.jsx

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,12 @@
+import './spinner.styles.scss'
+
+const Spinner = () => {
+    
+    return (
+        <div className="spinnerOverlay">
+            <div className="spinnerContainer"></div>
+        </div>
+    )
+}
+ 
+export default Spinner;
