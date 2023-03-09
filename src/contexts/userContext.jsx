f91107c6b919b32077bee0bdaf7a6e5be345950d
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-09 15:54:23 +0100

    Create userContext.jsx

---
+++ src/contexts/userContext.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [userDoc, setUserDoc] = useState({});
+
+  return (
+    <UserContext.Provider
+      value={{
+        userDoc,
+        setUserDoc,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
