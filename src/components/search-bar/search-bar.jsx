216a9666f6de6946f0c8809a1aa82f6ca73772c0
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-20 14:37:37 +0100

    Update search-bar.jsx

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
   const { handleChange } = useContext(SearchContext);
 
   return (
-    <div className="search-bar flex px-6 my-4 space-x-5">
+    <div className="search-bar flex px-6 my-4 mb-0 md:my-4 space-x-5">
       <img src={search} alt="" />
       <input
         type="text"
