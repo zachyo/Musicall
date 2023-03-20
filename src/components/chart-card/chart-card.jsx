3854362f252088a2b5a93cbe4cba19fc055e58b7
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-20 14:35:39 +0100

    Update chart-card.jsx

--- src/components/chart-card/chart-card.jsx
+++ src/components/chart-card/chart-card.jsx
@@ -4,23 +4,20 @@ import './chart-card.scss';
 const ChartCard = ({card}) => {
   const {title, artist, cover} = card
   return (
-    <div className="chart-card animate-slideright">
-      <div className="flex items-center">
-        <div className="img">
+    <div className="chart-card animate-slideright p-4 md:items-center md:p-4 md:w-[32.6vw]">
+      <div className="flex flex-col w-4/5 justify-between md:justify-none md:flex-row md:items-center">
+        <div className="img md:mr-8">
           <img src={cover} alt="cardImage" />
         </div>
-        <div className="info">
+        <div className="info w-[190px] md:w-auto">
           <h2 className="text-lg">{title}</h2>
-          <p className="opacity-50 mt-1 mb-2">{artist.name}</p>
-          <p>5mins</p>
+          <p className="opacity-50 mt-1 mb-6 md:mb-2">{artist.name}</p>
+          <p className="text-[14px]">5mins</p>
         </div>
       </div>
 
-      <div className="fav">
-        <img
-          src={heart}
-          alt="heart"
-        />
+      <div className="fav mt-3 md:m-0">
+        <img src={heart} alt="heart" />
       </div>
     </div>
   );
