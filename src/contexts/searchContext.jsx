332bc11c917b89d00761254c54175cc527b2c088
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-22 11:37:08 +0100

    Update searchContext.jsx

--- src/contexts/searchContext.jsx
+++ src/contexts/searchContext.jsx
@@ -5,12 +5,13 @@ const SearchContext = createContext();
 
 export const SearchProvider = ({ children }) => {
   const [searchKey, setSearchKey] = useState();
+  const [cardList, setCardList] = useState([])
   //searching system
   const handleChange = (event) => {
     setSearchKey(event.target.value);
   };
   return (
-    <SearchContext.Provider value={{ searchKey, handleChange }}>
+    <SearchContext.Provider value={{ searchKey, cardList, handleChange, setCardList }}>
       {children}
     </SearchContext.Provider>
   );
