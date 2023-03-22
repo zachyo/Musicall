0d2e4497587aca26a0e84d66c5bdc3a6cb1bd0ce
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-22 11:35:20 +0100

    Update chart-card.jsx

--- src/components/chart-card/chart-card.jsx
+++ src/components/chart-card/chart-card.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import heart from '../../assets/icons/Heart.svg'
 import './chart-card.scss';
 
-const ChartCard = ({card}) => {
-  const {title, artist, cover} = card
+const ChartCard = ({card, num}) => {
+  const {title, artist, cover,id} = card
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`${num}/album`)
+  }
   return (
     <div className="chart-card animate-slideright p-4 md:items-center md:p-4 md:w-[32.6vw]">
-      <div className="flex flex-col w-4/5 justify-between md:justify-none md:flex-row md:items-center">
+      <div className="flex flex-col w-4/5 justify-between md:justify-start md:flex-row md:items-center" onClick={handleClick}>
         <div className="img md:mr-8">
           <img src={cover} alt="cardImage" />
         </div>
