03d32e31d6f22b19ddbb4f0e0ff7810dc6bee535
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-24 16:19:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import leadImage from './assets/images/Lead-image.png'
 import Collections from './pages/collections/collections';
 import LoggedIn from './pages/login/loggedIn';
 import Select from './pages/login/select';
+import ArtistsAndAlbums from './pages/arstist-albums/artists-albums';
+import PlaylistsAndPods from './pages/playlists-pods/playlists-pods';
 
 function App() {
   return (
@@ -16,12 +18,14 @@ function App() {
       <Routes>
         <Route exact path="" element={<Overview chartB={leadImage} />}>
           <Route path="/" element={<Homepage />} />
-          <Route path=":id/album" element={<ChartAlbum />}>
+          <Route path="album/:id" element={<ChartAlbum />}>
             {/* <Route path=":id" element={<Collections />} /> */}
           </Route>
+          <Route path="artists" element={<ArtistsAndAlbums />} />
+          <Route path="playlists" element={<PlaylistsAndPods/>} />
           <Route path="collections" element={<Collections />} />
-          <Route path="signin" element={<Select/>} />
-          <Route path="loggedin" element={<LoggedIn/>} />
+          <Route path="signin" element={<Select />} />
+          <Route path="loggedin" element={<LoggedIn />} />
         </Route>
       </Routes>
     </div>
