38469094aa4aa7dcf81263430be75c15577036b5
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-31 10:26:17 +0100

    Update carousel.jsx

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -2,27 +2,33 @@ import CARDS_DATA from "./carousel.data";
 
 import "./carousel.scss";
 import { useContext } from "react";
-import {Link} from 'react-router-dom'
 import SearchContext from "../../contexts/searchContext";
 import NowPlayingContext from "../../contexts/nowPlayingContext";
 import { searchFilter } from "../../utilities/searchFilter";
+import useMusicallStore from "../../store/musicallStore";
 
-const Carousel = ({songData}) => {
-  const {searchKey} = useContext(SearchContext)
+const Carousel = ({ songData }) => {
+  const { searchKey } = useContext(SearchContext);
   const { handleNowPlaying } = useContext(NowPlayingContext);
   let newData = songData;
   if (searchKey) {
     newData = searchFilter(searchKey, CARDS_DATA);
     console.log(searchKey, newData);
   }
-  const songs = newData?.map((each,i) => {
+  const setCurrentTracklist = useMusicallStore(
+    (state) => state.setCurrentTracklist
+  );
+  const songs = newData?.map((each, i) => {
     return (
       <div
         className="releaseCard"
         key={each.id}
-        onClick={()=>handleNowPlaying(each,i)}
+        onClick={() => {
+          setCurrentTracklist(songData)
+          handleNowPlaying(each, i);
+        }}
       >
-        <img src={each.album.cover} alt="songCover" />
+        <img src={each.album.cover} alt='' />
         <h2 className="truncate">{each.title}</h2>
         <p className="">{each?.artist?.name}</p>
       </div>
@@ -31,9 +37,7 @@ const Carousel = ({songData}) => {
   // to={each.type === 'album' ? `album/${each.title}` : null}
 
   return (
-    <div className="carousel flex text-light text-left gap-5">
-      {songs}
-    </div>
+    <div className="carousel flex text-light text-left gap-5">{songs}</div>
   );
 };
 
